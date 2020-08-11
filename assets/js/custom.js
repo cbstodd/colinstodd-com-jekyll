6905ea81fcb805eb8979c727c00b8d5dbd872f3f
cbstodd <cbstodd@gmail.com>
2020-08-11 19:33:26 -0400

    Added copied javascript to copy buttons

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,31 +1,48 @@
 const humanSelect = document.getElementById('human');
 const correctSelection = document.getElementById('humanSelection');
-const submitBtn = document.getElementById('submitContactForm');
+const submitBtn = document.getElementById('contactSubmitBtn');
+const clipboard = new ClipboardJS('.copy-btn');
 
-humanSelect.addEventListener('click', (evt) => {
-	evt.preventDefault();
-	const _selectedValue = evt.target.value;
-	validateForm(_selectedValue);
-})
+clipboard.on('success', (e) => {    
+  e.trigger.innerHTML = `<span class="text-green"><i class="fad fa-clipboard-check"></i> Copied!</span>`;
+  setTimeout(() => {
+    e.clearSelection();
+    e.trigger.innerHTML = `<i class="fad fa-clipboard-list"></i> Copy`;    
+  }, 2000);  
+});
 
-function validateForm(selectedValue) {
-	if (selectedValue == 3) {
-		// Removes danger styling if user selected wrong answer.
-		humanSelect.classList.remove('isBot');
-		correctSelection.parentNode.classList.remove('isBotText');	
-		// Adds success styling if user selected right answer.
-		humanSelect.classList.add('isHuman');		
-		correctSelection.parentNode.classList.add('isHumanText');	
-		correctSelection.innerHTML = `<span style="background-color:#414C20!important;">Good, you're not a Bot!</span>`;
-		// Enables Button
-		submitBtn.disabled = false;
-		submitBtn.value = 'Submit Message';
-		
-	} else {
-		humanSelect.classList.add('isBot');
-		correctSelection.parentNode.classList.add('isBotText');			
-		
-		submitBtn.disabled = true;
-		submitBtn.value = 'Form still invalid';
-	}	
-}
+clipboard.on('error', (e) => {
+  e.trigger.innerHTML = `<span class="text-pink"><i class="fad fa-exclamation-triangle"></i> Oops, unable to copy</span>`;
+  setTimeout(() => {
+    e.clearSelection();
+    e.trigger.innerHTML = `<i class="fad fa-clipboard-list"></i> Copy`;    
+  }, 2000);  
+});
+
+// Contact form Bot Check
+const validateForm = selectedValue => {
+  if (selectedValue == 3) {
+    // Removes danger styling if user selected wrong answer.
+    humanSelect.classList.remove('isBot');
+    correctSelection.parentNode.classList.remove('isBotText');
+    // Adds success styling if user selected right answer.
+    humanSelect.classList.add('isHuman');
+    correctSelection.parentNode.classList.add('isHumanText');
+    correctSelection.innerHTML = `<span style="background-color:#414C20!important;">Human! Good, you're not a Bot!</span>`;
+    // Enables Button
+    submitBtn.disabled = false;
+    submitBtn.value = 'Submit Message';
+  } else {
+    humanSelect.classList.add('isBot');
+    correctSelection.parentNode.classList.add('isBotText');
+
+    submitBtn.disabled = true;
+    submitBtn.value = 'Form still invalid';
+  }
+};
+
+humanSelect.addEventListener('click', evt => {
+  evt.preventDefault();
+  const _selectedValue = evt.target.value;
+  validateForm(_selectedValue);
+});
