7ee18a823a991ddb2276af7e85c226e30e5994fc
cbstodd <cbstodd@gmail.com>
2020-09-10 05:16:24 -0400

    Made some progress on refactoring reCATPCHA and custom bot validation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,10 @@
 const clipboard = new ClipboardJS('.copy-btn');
 const appleUrl = new ClipboardJS('.apple-btn');
 const humanSelect = document.getElementById('isHuman');
+const botSelect = document.getElementById('notHuman');
 const botIcon = document.getElementById('botIcon');
 const submitBtn = document.getElementById('contactSubmitBtn');
+const checkAnswerBtn = document.getElementById('checkAnswerBtn');
 // End Constant imports.
 
 clipboard.on('success', e => {
@@ -40,18 +42,43 @@ function calendarCopy(html, activeHtml, length) {
 }
 
 function validateHuman(selectedValue) {
-  if (selectedValue) {
-    botIcon.classList.remove('fa-user-robot');
-    botIcon.classList.add('fa-user');
+  console.log('validateHuman', selectedValue);
+  if (selectedValue === true) {
     submitBtn.disabled = false;
     submitBtn.value = 'Submit Message';
   } else {
-    botIcon.classList.remove('fa-user');
-    botIcon.classList.add('fa-user-robot');
     submitBtn.disabled = true;
     submitBtn.value = 'Form Invalid';
   }
 }
 
+checkAnswerBtn.addEventListener('mouseup', () => {
+  const questAnswer = document.getElementById('questAnswer').value;
+  const realAnswer = site.correctBotAnswer;
+  console.log(questAnswer);
+  if (questAnswer && questAnswer.toLowerCase() == realAnswer) {
+    validateHuman(true);
+  } else {
+    validateHuman(false);
+  }
+});
+
+// botToggleForm.className.add('bot-select');
+
+// botToggleForm.className.remove('show-bot-select');
+
 // Returns boolean value if checked or not.
-humanSelect.addEventListener('click', evt => validateHuman(evt.target.checked));
+humanSelect.addEventListener('touch', evt => {
+  validateHuman(evt.target.checked);
+  showCustomBotSelect();
+});
+
+function showCustomBotSelect() {
+  // botToggleForm.add.className('show-bot-select');
+  humanSelect.remove.className('hide-bot-select');
+};
+
+// botSelect.addEventListener('click', evt => {
+//   evt.preventDefault();
+//   return isBot(evt.target.checked);
+// });
